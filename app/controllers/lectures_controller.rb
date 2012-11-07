05339c5c37541266a5e8446131664be9f0833a8a
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2012-11-07 16:23:45 -0500

    updated paperclip

---
+++ app/controllers/lectures_controller.rb
@@ -0,0 +1,30 @@
+class LecturesController < ApplicationController
+	def new
+		@lecture = Lecture.new
+	end
+
+	def create
+		@lecture = Lecture.create(params[:lecture])
+		@lecture.save
+		redirect_to @lecture
+	end
+
+	def show
+		@lecture = Lecture.find(params[:id])
+	end
+
+	def play
+		@lecture = Lecture.find(params[:id])
+		@audio_lesson_url = @lecture.sound_file.url
+	end
+
+	def edit
+	end
+
+	def delete
+	end
+
+	def show
+		@lectures = Lecture.all
+	end
+end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,4 +1,6 @@
 class Lecture < ActiveRecord::Base
+	attr_accessible :name, :description, :sound_file, :sound_file_file_name
+
 	has_attached_file :sound_file,
      :storage => :s3,
      :s3_credentials => "#{Rails.root}/config/s3.yml",
