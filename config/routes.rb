02dfa77fe7e055dad6f0b7b9622eb1de5c1b8e98
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2012-11-07 15:47:09 -0500

    added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ DemoApp::Application.routes.draw do
   post "calls/flow"
   post "calls/exception"
   root :to => "calls#index"
+
+  resources :lectures, :only => [:new, :create, :show]
+
+  match ':controller(/:action(.:format))'
+  match ':controller(/:action(/:id(.:format)))'
 end
+
