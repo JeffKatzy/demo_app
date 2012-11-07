5ef1da941ab2215de43decaeb8b392a2c9ab7b01
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2012-11-07 12:01:07 -0500

    add the state column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106204730) do
+ActiveRecord::Schema.define(:version => 20121107165822) do
 
   create_table "calls", :force => true do |t|
     t.string   "to"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20121106204730) do
     t.string   "caller"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "state"
   end
 
 end
