a9d0c9c0dd87edc11403b6da399fff9c0a2dfa0b
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2012-11-12 19:33:33 -0500

    changed migrations on answer_id

--- db/migrate/20121109163716_remove_question_id_from_users.rb
+++ db/migrate/20121109163716_remove_question_id_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveQuestionIdFromUsers < ActiveRecord::Migration
   def up
     remove_column :users, :question_id
-    remove_column :users, :answer_id
+    #remove_column :users, :answer_id
   end
 
   def down
