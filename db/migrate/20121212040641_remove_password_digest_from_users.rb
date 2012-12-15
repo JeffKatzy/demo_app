8eca8cc3ca7490ecf337e9f4f81f16dadda125a3
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2012-12-14 22:40:47 -0500

    remove the file removing digest from users

--- db/migrate/20121212040641_remove_password_digest_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemovePasswordDigestFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :password_digest
-  end
-
-  def down
-    add_column :users, :password_digest, :string
-  end
-end
