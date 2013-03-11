5ac40208e17ebc1548a5ded063516cb23fb5cfcd
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-03-11 15:32:19 -0400

    added current teacher present

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,15 @@
 module UsersHelper
 
 	def performance(object)
-		# if object.user_answers.grade == "mastered"
-		# 	"success"
-		# elsif object.user_answers.grade == "passed"
-		# 	"secondary"
-		# elsif object.user_answers.grade == "failed"
-		# 	"alert"
-		# elsif object.user_answers.grade == "no answers"
-		# 	"radius"
-		# end
+		if object.user_answers.grade == "mastered"
+			"success"
+		elsif object.user_answers.grade == "passed"
+			"secondary"
+		elsif object.user_answers.grade == "failed"
+			"alert"
+		elsif object.user_answers.grade == "no answers"
+			"radius"
+		end
 	end
 
 	def correct?(answer)
