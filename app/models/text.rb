c62f6042380ddc9adf1bd5ff1e04d0a0c2559deb
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-04-25 11:03:05 -0400

    changed the twilio number

--- app/models/text.rb
+++ app/models/text.rb
@@ -3,7 +3,7 @@ class Text < ActiveRecord::Base
   def self.send_text_to phone, body
     @client = Twilio::REST::Client.new(ENV['TW_SID'], ENV['TW_TOK'])
     @account = @client.account
-    @account.sms.messages.create(:from => '+12159702907', :to => phone, :body => body)
+    @account.sms.messages.create(:from => '+12158746778', :to => phone, :body => body)
   end
 
 end
