229fbe1e834362ff7c6951318646635fe3544750
Toby Matejovsky <toby.matejovsky@gmail.com>
2013-08-27 09:29:50 -0400

    Configure twilio number from environment variables

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,1 +1,6 @@
-TWILIO_NUMBER = '+12158746778'
+TWILIO_NUMBER = ENV['TW_NUM']
+# To configure for Heroku:
+#   heroku config:add TW_NUM=+12158746778
+# To configure in local terminal session:
+#   export TW_NUM=+12158746778
+# You could also put that in your ~/.bash_profile so it runs automatically for each new terminal tab.
